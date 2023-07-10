e0c16ea0c00e90f6b23fb2a4fd53d7466b6fa852
Dedyk Suntoro Atmojo <dedyk.ds@gmail.com>
2023-07-10 21:53:59 +0700

    Init

---
+++ auth-login.php
@@ -0,0 +1,81 @@
+<?php 
+include 'konfigurasi.php';
+
+session_start();
+if (isset($_SESSION['email'])) {
+    header("Location: index.php");
+}
+
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM tbl_pengguna WHERE email='$email'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $verify = password_verify($password, $row['password']);
+  
+    if ($verify) {
+        $_SESSION['email'] = $row['email'];
+        header("Location: index.php");
+    } else {
+        echo "<script>alert('Email atau password Anda salah. Silahkan coba lagi!')</script>";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Mazer Admin Dashboard</title>
+    <link rel="stylesheet" href="assets/css/main/app.css">
+    <link rel="stylesheet" href="assets/css/pages/auth.css">
+    <link rel="shortcut icon" href="assets/images/logo/favicon.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/png">
+</head>
+
+<body>
+    <div id="auth">
+        
+<div class="row h-100">
+    <div class="col-lg-5 col-12">
+        <div id="auth-left">
+            <div class="auth-logo">
+                <a href="index.html"><img src="assets/images/logo/logo.svg" alt="Logo"></a>
+            </div>
+            <h1 class="auth-title">Log in.</h1>
+            <p class="auth-subtitle mb-5">Masuk dengan akun yang telah terdaftar.</p>
+
+            <form action="" method="POST" autocomplete="off">
+                <div class="form-group position-relative has-icon-left mb-4">
+                    <input type="email" class="form-control form-control-xl" placeholder="Email" name="email">
+                    <div class="form-control-icon">
+                        <i class="bi bi-person"></i>
+                    </div>
+                </div>
+                <div class="form-group position-relative has-icon-left mb-4">
+                    <input type="password" class="form-control form-control-xl" placeholder="Password" name="password">
+                    <div class="form-control-icon">
+                        <i class="bi bi-shield-lock"></i>
+                    </div>
+                </div>
+                <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5" name="submit">Log in</button>
+            </form>
+            <div class="text-center mt-5 text-lg fs-4">
+                <p class="text-gray-600">Created by Artmedia</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-7 d-none d-lg-block">
+        <div id="auth-right">
+
+        </div>
+    </div>
+</div>
+
+    </div>
+</body>
+
+</html>
