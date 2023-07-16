343808549fbade125c2f310bb0e13478a07656ba
Dedyk Suntoro Atmojo <dedyk.ds@gmail.com>
2023-07-16 23:36:10 +0700

    Modul mobil belum selesai

---
+++ mobil-create.php
@@ -0,0 +1,205 @@
+<?php
+include 'konfigurasi.php';
+session_start();
+
+if (!isset($_SESSION['email'])) {
+    header("Location: auth-login.php");
+}
+
+if ($_GET['mobil'] != 'active') {
+    header("Location: error-404.php");
+}
+
+if (isset($_POST['submit'])) {
+    $pabrikan = $_POST['pabrikan'];
+    $merek = $_POST['merek'];
+    $tahun = $_POST['tahun'];
+    $nomor_rangka = $_POST['nomor_rangka'];
+    $nomor_mesin = $_POST['nomor_mesin'];
+    $nopol = $_POST['nopol'];
+    $pemilik = $_POST['pemilik'];
+    $alamat = $_POST['alamat'];
+    $kantor = $_POST['kantor'];
+    $harga_perbulan = $_POST['harga_perbulan'];
+    $harga_perminggu = $_POST['harga_perminggu'];
+    $harga_perhari = $_POST['harga_perhari'];
+    $harga_perjam = $_POST['harga_perjam'];
+    $status = $_POST['status'];
+    $created_at = date("Y-m-d h:i:s");
+
+    $sql = "INSERT INTO tbl_mobil (pabrikan, merek, tahun, nomor_rangka, nomor_mesin, nopol, pemilik, alamat, kantor, harga_perbulan, harga_perminggu, harga_perhari, harga_perjam, `status`, created_at)
+            VALUES ($pabrikan, '$merek', $tahun, '$nomor_rangka', '$nomor_mesin', '$nopol', '$pemilik', '$alamat', $kantor, $harga_perbulan, $harga_perminggu, $harga_perhari, $harga_perjam, '$status', '$created_at')";
+
+    if (mysqli_query($conn, $sql)) {
+        mysqli_close($conn);
+        $_SESSION['pesannya'] = '<div class="alert alert-light-success color-success"><i class="bi bi-check-circle"></i> Data berhasil ditambahkan.</div>';
+        header('Location: mobil.php?mobil=active');
+    } else {
+        var_dump("Error: " . $sql . "<br>" . mysqli_error($conn));
+        mysqli_close($conn);
+        $_SESSION['pesannya'] = '<div class="alert alert-light-danger color-danger"><i class="bi bi-exclamation-circle"></i> Data gagal ditambahkan.</div>';
+        header('Location: mobil.php?mobil=active');
+    }
+}
+
+$sql_kantor = "SELECT * FROM tbl_kantor";
+$result_kantor = mysqli_query($conn, $sql_kantor);
+
+$sql_pabrikan = "SELECT * FROM tbl_pabrikan";
+$result_pabrikan = mysqli_query($conn, $sql_pabrikan);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'header.php'; ?>
+
+<body>
+    <div id="app">
+        <?php include 'sidebar.php'; ?>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+
+            <div class="page-heading">
+                <h3>Tambah Mobil</h3>
+            </div>
+            <section class="section">
+                <div class="row" id="table-hover-row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-content">
+                                <div class="card-body">
+                                    <form class="form" method="POST" action="" data-parsley-validate>
+                                        <div class="row">
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="pabrikan-column">Pabrikan</label>
+                                                    <select class="form-select" id="pabrikan-column" name="pabrikan" data-parsley-required="true">
+                                                        <option value="" selected disabled>-- Pilih --</option>
+                                                        <?php
+                                                        while ($row_pabrikan = mysqli_fetch_array($result_pabrikan)) {
+                                                            echo "<option value='" . $row_pabrikan['id'] . "'>" . $row_pabrikan['pabrikan'] . "</option>";
+                                                        }
+                                                        ?>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="merek-mobil-column">Merek Mobil</label>
+                                                    <input type="text" id="merek-mobil-column" class="form-control" placeholder="Masukkan merek mobil" name="merek" data-parsley-required="true">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="tahun-mobil-column">Tahun Mobil</label>
+                                                    <input type="number" id="tahun-mobil-column" class="form-control" placeholder="Masukkan tahun mobil" name="tahun" data-parsley-required="true">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="nomor-rangka-column">Nomor Rangka</label>
+                                                    <input type="number" id="nomor-rangka-column" class="form-control" placeholder="Masukkan nomor rangka" name="nomor_rangka" data-parsley-required="true">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="nomor-mesin-column">Nomor Mesin</label>
+                                                    <input type="number" id="nomor-mesin-column" class="form-control" placeholder="Masukkan nomor mesin" name="nomor_mesin" data-parsley-required="true">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="nopol-mobil-column">Nopol</label>
+                                                    <input type="text" id="nopo-mobil-column" class="form-control" placeholder="Masukkan nopol mobil" name="nopol" data-parsley-required="true">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="pemilik-mobil-column">Pemilik</label>
+                                                    <input type="text" id="pemilik-mobil-column" class="form-control" placeholder="Masukkan pemilik mobil" name="pemilik" data-parsley-required="true">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="alamat-pemilik-column">Alamat</label>
+                                                    <input type="text" id="alamat-pemilik-column" class="form-control" placeholder="Masukkan alamat pemilik" name="alamat" data-parsley-required="true">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="kantor-column">Kantor</label>
+                                                    <select class="form-select" id="kantor-column" name="kantor" data-parsley-required="true">
+                                                        <option value="" selected disabled>-- Pilih --</option>
+                                                        <?php
+                                                        while ($row_kantor = mysqli_fetch_array($result_kantor)) {
+                                                            echo "<option value='" . $row_kantor['id'] . "'>" . $row_kantor['nama_kantor'] . "</option>";
+                                                        }
+                                                        ?>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="harga-perbulan-column">Harga Sewa Perbulan</label>
+                                                    <input type="number" id="harga-perbulan-column" class="form-control" placeholder="Masukkan harga sewa perbulan" name="harga_perbulan" data-parsley-required="false">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="harga-perminggu-column">Harga Sewa Perminggu</label>
+                                                    <input type="number" id="harga-perminggu-column" class="form-control" placeholder="Masukkan harga sewa perminggu" name="harga_perminggu" data-parsley-required="false">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="harga-perhari-column">Harga Sewa Perhari</label>
+                                                    <input type="number" id="harga-perhari-column" class="form-control" placeholder="Masukkan harga sewa perhari" name="harga_perhari" data-parsley-required="false">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="harga-perjam-column">Harga Sewa Perjam</label>
+                                                    <input type="number" id="harga-perjam-column" class="form-control" placeholder="Masukkan harga sewa perjam" name="harga_perjam" data-parsley-required="false">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="status-column">Status</label>
+                                                    <select class="form-select" id="status-column" name="status" data-parsley-required="true">
+                                                        <option value="" selected disabled>-- Pilih --</option>
+                                                        <option value="Disewakan">Disewakan</option>
+                                                        <option value="Dikantor">Dikantor</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-12 d-flex justify-content-end">
+                                                <button name="submit" class="btn btn-primary me-1 mb-1">Simpan</button>
+                                                <a href="mobil.php?mobil=active" class="btn btn-light-secondary me-1 mb-1">Batal</a>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <?php include 'footer.php'; ?>
+        </div>
+    </div>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/app.js"></script>
+
+    <script src="assets/extensions/jquery/jquery.min.js"></script>
+    <script src="assets/extensions/parsleyjs/parsley.min.js"></script>
+    <script src="assets/js/pages/parsley.js"></script>
+
+</body>
+
+</html>
---
+++ mobil.php
@@ -0,0 +1,179 @@
+<?php
+include 'konfigurasi.php';
+session_start();
+
+if (!isset($_SESSION['email'])) {
+    header("Location: auth-login.php");
+}
+
+if ($_GET['mobil'] != 'active') {
+    header("Location: error-404.php");
+}
+
+if (isset($_POST['delete'])) {
+    $id = $_POST['delete'];
+
+    $sql_delete = "DELETE FROM tbl_mobil WHERE id = $id";
+
+    if (mysqli_query($conn, $sql_delete)) {
+        $_SESSION['pesannya'] = '<div class="alert alert-light-success color-success"><i class="bi bi-check-circle"></i> Data berhasil dihapus.</div>';
+    } else {
+        // var_dump("Error: " . $sql . "<br>" . mysqli_error($conn));
+        $_SESSION['pesannya'] = '<div class="alert alert-light-danger color-danger"><i class="bi bi-exclamation-circle"></i> Data gagal dihapus.</div>';
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'header.php'; ?>
+
+<body>
+    <div id="app">
+        <?php include 'sidebar.php'; ?>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+
+            <div class="page-heading">
+                <h3>Mobil</h3>
+            </div>
+            <section class="section">
+                <div class="row" id="table-hover-row">
+                    <div class="col-12">
+                        <?php
+                        if (isset($_SESSION['pesannya'])) {
+                            echo $_SESSION['pesannya'];
+                            unset($_SESSION['pesannya']);
+                        }
+                        ?>
+                        <div class="card">
+                            <div class="card-header">
+                                <a href="mobil-create.php?mobil=active" class="btn icon icon-left btn-primary"><i data-feather="plus"></i> Tambah Data</a>
+                            </div>
+                            <div class="card-content">
+                                <div class="card-body">
+                                    <?php
+                                    $sql = "SELECT
+                                                tbl_mobil.id,
+                                                tbl_pabrikan.pabrikan AS pabrikan,
+                                                tbl_mobil.merek,
+                                                tbl_mobil.tahun,
+                                                tbl_mobil.nomor_rangka,
+                                                tbl_mobil.nomor_mesin,
+                                                tbl_mobil.nopol,
+                                                tbl_mobil.pemilik,
+                                                tbl_mobil.alamat,
+                                                tbl_kantor.nama_kantor AS kantor,
+                                                tbl_mobil.harga_perbulan,
+                                                tbl_mobil.harga_perminggu,
+                                                tbl_mobil.harga_perhari,
+                                                tbl_mobil.harga_perjam,
+                                                tbl_mobil.status
+                                            FROM
+                                                tbl_mobil
+                                            LEFT JOIN tbl_pabrikan ON tbl_pabrikan.id = tbl_mobil.pabrikan
+                                            LEFT JOIN tbl_kantor ON tbl_kantor.id = tbl_mobil.kantor";
+                                    $result = mysqli_query($conn, $sql);
+                                    $number = 1;
+                                    if (mysqli_num_rows($result) > 0) {
+                                        echo '<div class="table-responsive">';
+                                        echo '<table class="table table-hover" id="table1">';
+                                        echo '<thead>
+                                                <tr>
+                                                    <th>NO</th>
+                                                    <th>PABRIKAN</th>
+                                                    <th>MEREK</th>
+                                                    <th>TAHUN</th>
+                                                    <th>NO RANGKA</th>
+                                                    <th>NO MESIN</th>
+                                                    <th>NOPOL</th>
+                                                    <th>PEMILIK</th>
+                                                    <th>ALAMAT</th>
+                                                    <th>KANTOR</th>
+                                                    <th>PERBULAN</th>
+                                                    <th>PERMINGGU</th>
+                                                    <th>PERHARI</th>
+                                                    <th>PERJAM</th>
+                                                    <th>STATUS</th>
+                                                    <th class="text-center" colspan="2">TINDAKAN</th>
+                                                </tr>
+                                            </thead>';
+                                        echo '<tbody>';
+                                        while ($row = mysqli_fetch_assoc($result)) {
+                                            echo "<tr>
+                                                    <td style='width:1%'>" . $number . "</td>
+                                                    <td>" . $row['pabrikan'] . "</td>
+                                                    <td>" . $row['merek'] . "</td>
+                                                    <td>" . $row['tahun'] . "</td>
+                                                    <td>" . $row['nomor_rangka'] . "</td>
+                                                    <td>" . $row['nomor_mesin'] . "</td>
+                                                    <td>" . $row['nopol'] . "</td>
+                                                    <td>" . $row['pemilik'] . "</td>
+                                                    <td>" . $row['alamat'] . "</td>
+                                                    <td>" . $row['kantor'] . "</td>
+                                                    <td>" . $row['harga_perbulan'] . "</td>
+                                                    <td>" . $row['harga_perminggu'] . "</td>
+                                                    <td>" . $row['harga_perhari'] . "</td>
+                                                    <td>" . $row['harga_perjam'] . "</td>
+                                                    <td>" . $row['status'] . "</td>
+                                                    <td style='width:1%' class='text-center'>
+                                                        <form class='form' method='POST' action='mobil-edit.php?mobil=active'>
+                                                            <button name='edit' value='" . $row['id'] . "' class='btn btn-sm icon btn-success'><i class='bi bi-pencil'></i></button>
+                                                        </form>
+                                                    </td>
+                                                    <td style='width:1%' class='text-center'>
+                                                        <form class='form' method='POST' action=''>
+                                                            <div class='btn-group dropstart'>
+                                                                <button class='btn btn-sm btn-danger icon dropdown-toggle' type='button' id='dropdownMenuDelete' data-bs-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                                                                    <i class='bi bi-x'></i>
+                                                                </button>
+                                                                <div class='dropdown-menu' aria-labelledby='dropdownMenuDelete'>
+                                                                    <button name='delete' value='" . $row['id'] . "' class='dropdown-item'>Ya</button>
+                                                                    <button class='dropdown-item'>Tidak</button>
+                                                                </div>
+                                                            </div>
+                                                        </form>
+                                                    </td>
+                                                </tr>";
+                                            $number++;
+                                        }
+                                        echo '</tbody>';
+                                        echo '</table>';
+                                        echo '</div>';
+                                    } else {
+                                        echo "
+                                            <div class='alert alert-warning'>
+                                                <h4 class='alert-heading'>Data Kosong</h4>
+                                                <p>Tidak ada data untuk ditampilkan.</p>
+                                            </div>
+                                        ";
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <?php include 'footer.php'; ?>
+        </div>
+    </div>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/app.js"></script>
+
+    <!-- Need: Apexcharts -->
+    <script src="assets/extensions/apexcharts/apexcharts.min.js"></script>
+    <script src="assets/js/pages/dashboard.js"></script>
+
+    <script src="assets/extensions/simple-datatables/umd/simple-datatables.js"></script>
+    <script src="assets/js/pages/simple-datatables.js"></script>
+
+</body>
+
+</html>
