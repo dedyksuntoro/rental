d56c9aa2661296a2ebbd901bceb71969d92b99b2
Dedyk Suntoro Atmojo <dedyk.ds@gmail.com>
2023-07-18 00:20:38 +0700

    Fix bug login

--- auth-login.php
+++ auth-login.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include 'konfigurasi.php';
 
 session_start();
@@ -13,13 +13,15 @@ if (isset($_POST['submit'])) {
     $sql = "SELECT * FROM tbl_pengguna WHERE email='$email'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
-    $verify = password_verify($password, $row['password']);
-  
-    if ($verify) {
-        $_SESSION['email'] = $row['email'];
-        header("Location: index.php?index=active");
-    } else {
-        echo "<script>alert('Email atau password Anda salah. Silahkan coba lagi!')</script>";
+    if (isset($row['password'])) {
+        $verify = password_verify($password, $row['password']);
+
+        if ($verify) {
+            $_SESSION['email'] = $row['email'];
+            header("Location: index.php?index=active");
+        } else {
+            echo "<script>alert('Email atau password Anda salah. Silahkan coba lagi!')</script>";
+        }
     }
 }
 ?>
@@ -38,42 +40,42 @@ if (isset($_POST['submit'])) {
 
 <body>
     <div id="auth">
-        
-<div class="row h-100">
-    <div class="col-lg-5 col-12">
-        <div id="auth-left">
-            <div class="auth-logo">
-                <a href="index.html"><img src="assets/images/logo/logo.svg" alt="Logo"></a>
-            </div>
-            <h1 class="auth-title">Log in.</h1>
-            <p class="auth-subtitle mb-5">Masuk dengan akun yang telah terdaftar.</p>
 
-            <form action="" method="POST" autocomplete="off">
-                <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="email" class="form-control form-control-xl" placeholder="Email" name="email">
-                    <div class="form-control-icon">
-                        <i class="bi bi-person"></i>
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                        <a href="index.html"><img src="assets/images/logo/logo.svg" alt="Logo"></a>
+                    </div>
+                    <h1 class="auth-title">Log in.</h1>
+                    <p class="auth-subtitle mb-5">Masuk dengan akun yang telah terdaftar.</p>
+
+                    <form action="" method="POST" autocomplete="off">
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="email" class="form-control form-control-xl" placeholder="Email" name="email">
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl" placeholder="Password" name="password">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5" name="submit">Log in</button>
+                    </form>
+                    <div class="text-center mt-5 text-lg fs-4">
+                        <p class="text-gray-600">Created by Artmedia</p>
                     </div>
                 </div>
-                <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="password" class="form-control form-control-xl" placeholder="Password" name="password">
-                    <div class="form-control-icon">
-                        <i class="bi bi-shield-lock"></i>
-                    </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
+
                 </div>
-                <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5" name="submit">Log in</button>
-            </form>
-            <div class="text-center mt-5 text-lg fs-4">
-                <p class="text-gray-600">Created by Artmedia</p>
             </div>
         </div>
-    </div>
-    <div class="col-lg-7 d-none d-lg-block">
-        <div id="auth-right">
-
-        </div>
-    </div>
-</div>
 
     </div>
 </body>
